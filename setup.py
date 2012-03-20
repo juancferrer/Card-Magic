20a09af766f1a9ff7292b9b31e23aa77352f2426
Juan Carlos Ferrer <contact@micronixsolutions.com>
2012-03-19 22:18:46 -0500

    Added setuptools install script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Card-Magic',
+      version='1.0',
+      description='The best card and decks ever',
+      author='Juan Carlos Ferrer',
+      author_email='juan.carlos@micronixsolutions.com',
+      packages=['cardmagic', 'cardmagic.tests'],
+     )
